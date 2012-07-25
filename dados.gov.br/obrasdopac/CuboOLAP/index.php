3dfb6279c9fbc501e3183be598be1b51fa588808
Caio Moreno de Souza <caiomsouza@gmail.com>
2012-07-24 22:08:47 -0300

    deleted obrasdopac

--- dados.gov.br/obrasdopac/CuboOLAP/index.php
+++
@@ -1,4 +0,0 @@
-<html>
-<title>Aplicativo - Obras do PAC by IT4biz e Dados.gov.br</title>
-<iframe src="http://it4bizbiservices.com/pentaho/content/saiku-ui/index.html?biplugin=true&userid=obrasdopac&password=it4biz" width="100%" height="100%" frameborder="0"></iframe>
-</html>
--- dados.gov.br/obrasdopac/PaineldeControledosEmpreendimentos/index.php
+++
@@ -1,4 +0,0 @@
-<html>
-<title>Aplicativo - Obras do PAC by IT4biz e Dados.gov.br</title>
-<iframe src="http://it4bizbiservices.com/pentaho/content/pentaho-cdf-dd/Render?solution=PAC&path=%2FDashboards&file=Painel+de+Controle+dos+Empreendimentos.wcdf&userid=obrasdopac&password=it4biz" width="100%" height="100%" frameborder="0"></iframe>
-</html>
